d78cff622f1c733d09919ec2f0d0491ff15e2ca4
James Pederson <james@jpederson.com>
2016-09-26 15:34:42 -0500

    Adjust the forms and hide the rate field if it's not a refinance. Adjust JS and styles accordingly.

--- js/src/calculator.js
+++ js/src/calculator.js
@@ -7,46 +7,74 @@ $(function(){
 		$(this).val( fixed );
 	});
 
+	$("input.type").change(function(){
+		if ( $(this).val() == "purchase" ) {
+			$(".rate-field").slideUp();
+			$(".calculator .rate").val( "" );
+		} else {
+			$(".rate-field").slideDown();
+		}
+	});
+
 	$(".calculate").click(function(){
-		$(".tool").slideUp( 'slow' );
-		$(".results").slideDown( 'slow' );
-		$('html,body').animate({
-			scrollTop: $( "header img" ).height()
-        }, 800);
+		if ( $(".calculator .amount").val() > 0 ) {
 
-		var loan_info = $.loanInfo({
-			amount: $(".calculator .amount").val(),
-			rate: $(".calculator .rate").val(),
-			term: $(".calculator .term").val()
-		});
+			// hide the error div in case it's visible
+			$(".error").hide();
 
-		var loan_info_compare = $.loanInfo({
-			amount: $(".calculator .amount").val(),
-			rate: $(".calculator .rate_compare").val(),
-			term: $(".calculator .term").val()
-		});
+			// slide closed the tool, and open the results
+			$(".tool").slideUp( 'slow' );
+			$(".results").slideDown( 'slow' );
+			$('html,body').animate({
+				scrollTop: $( "header img" ).height()
+	        }, 800);
 
-		var interest_savings = loan_info.total_interest - loan_info_compare.total_interest;
-		interest_savings = ( Math.ceil( interest_savings * 100 ) / 100 );
+			// calculate the loan with the CU rate
+			var loan_info_compare = $.loanInfo({
+				amount: $(".calculator .amount").val(),
+				rate: $(".calculator .rate_compare").val(),
+				term: $(".calculator .term").val()
+			});
 
-		var one_percent = loan_info_compare.original_amount * .01;
+			// initially set the interest savings to 0 by default
+			var interest_savings = 0;
 
-		var three_months = Math.ceil( ( loan_info_compare.payment_amount * 300 ) ) / 100;
+			// if the rate is set (it's a refinance) calculate a comparison loan
+			if ( $(".calculator .rate").val() > 0 ) {
 
-		var total_savings = interest_savings + one_percent + three_months;
+				var loan_info = $.loanInfo({
+					amount: $(".calculator .amount").val(),
+					rate: $(".calculator .rate").val(),
+					term: $(".calculator .term").val()
+				});
+				interest_savings = loan_info.total_interest - loan_info_compare.total_interest;
+				interest_savings = ( Math.ceil( interest_savings * 100 ) / 100 );
 
-		// log the calculation data for test purposes
-		console.log( loan_info );
-		console.log( loan_info_compare );
-		console.log( interest_savings );
-		console.log( one_percent );
-		console.log( three_months );
-		console.log( total_savings );
+				$(".interest-savings-text").show();
 
-		$(".results .interest-savings").html( interest_savings );
-		$(".results .cash-back").html( one_percent );
-		$(".results .no-payments").html( three_months );
-		$(".results .total-savings").html( total_savings );
+			} else {
+
+				$(".interest-savings-text").hide();
+
+			}
+
+			// calculate one percent cash back
+			var one_percent = loan_info_compare.original_amount * .01;
+
+			// calculate three months of no payments
+			var three_months = Math.ceil( ( loan_info_compare.payment_amount * 300 ) ) / 100;
+
+			// calculate total savings
+			var total_savings = interest_savings + one_percent + three_months;
+
+			// set values in results spans.
+			$(".results .interest-savings").html( interest_savings );
+			$(".results .cash-back").html( one_percent );
+			$(".results .no-payments").html( three_months );
+			$(".results .total-savings").html( total_savings );
+		} else {
+			$(".error").slideDown();
+		}
 	});
 
 	$(".go-back").click(function(){
